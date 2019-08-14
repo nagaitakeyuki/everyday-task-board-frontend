a1e6d7e23225adf2803ab51c07b27a719dcd2ff5
nagaitakeyuki <goodingmorning@yahoo.co.jp>
2019-08-14 21:05:02 +0900

    correctly sort when changing story belonging and changing task status.

--- app/reducer/sprints.js
+++ app/reducer/sprints.js
@@ -6,50 +6,6 @@ const initState = {
   currentSprint: undefined
 }
 
-const copySprintsMap = srcSprintsMap => {
-  const copiedSprintsMap = new Map()
-
-  for (const [srcSprintId, srcSprint] of srcSprintsMap.entries()) {
-    const copiedStories = new Map()
-
-    for (const [srcStoryId, srcStory] of srcSprint.stories.entries()) {
-      const copiedTasks = new Map()
-
-      for (const [srcTaskId, srcTask] of srcStory.tasks.entries()) {
-        copiedTasks.set(srcTaskId, {...srcTask})
-      }
-
-      copiedStories.set(srcStoryId, {...srcStory, tasks: copiedTasks})
-    }
-
-    copiedSprintsMap.set(srcSprintId, {...srcSprint, stories: copiedStories})
-  }
-
-  return copiedSprintsMap
-}
-
-const copyBacklogCategoriesMap = srcBacklogCategoriesMap => {
-  const copiedBacklogCategoriesMap = new Map()
-
-  for (const [srcBacklogCategoryId, srcBacklogCategory] of srcBacklogCategoriesMap.entries()) {
-    const copiedStories = new Map()
-
-    for (const [srcStoryId, srcStory] of srcBacklogCategory.stories.entries()) {
-      const copiedTasks = new Map()
-
-      for (const [srcTaskId, srcTask] of srcStory.tasks.entries()) {
-        copiedTasks.set(srcTaskId, {...srcTask})
-      }
-
-      copiedStories.set(srcStoryId, {...srcStory, tasks: copiedTasks})
-    }
-
-    copiedBacklogCategoriesMap.set(srcBacklogCategoryId, {...srcBacklogCategory, stories: copiedStories})
-  }
-
-  return copiedBacklogCategoriesMap
-}
-
 export default (state = initState, action) => {
   switch (action.type) {
     case Types.SET_SPRINTS: {
@@ -186,16 +142,7 @@ export default (state = initState, action) => {
 
       // 　2. 移動先のスプリント or バックログカテゴリーのストーリー群
       Array.from(dest.stories.values())
-        .sort((a, b) => {
-          // ステータス変更されたタスクを優先的に前に並べる
-          if(a.sortOrder === b.sortOrder) {
-            if (a.storyId === storyId) return -1
-            if (b.storyId === storyId) return 1
-          } 
-          
-          // その他の場合は単純に昇順に並べる
-          return a.sortOrder - b.sortOrder
-        })
+        .sort((a, b) => sortItems(a, a.storyId, b, b.storyId, storyId))
         .forEach((story, index) => (story.sortOrder = index))
 
       return { ...state, sprints: copiedSprints, backlogCategories: copiedBacklogCategories}
@@ -216,22 +163,8 @@ export default (state = initState, action) => {
 
       // 表示順を再設定する
       Array.from(src.stories.values())
-        .sort((a, b) => {
-          // 同じ表示順の場合、順番の変更方向によって対象ストーリーを優先的に前 or 後に並べる
-          if(a.sortOrder === b.sortOrder){
-            if (isUpForward) {
-              if(a.storyId === storyId) return 1
-              if(b.storyId === storyId) return -1
-            } else {
-              if(a.storyId === storyId) return -1
-              if(b.storyId === storyId) return 1
-            }
-          } 
-          
-          // その他の場合は単純に昇順に並べる
-          return a.sortOrder - b.sortOrder
-        })
-        .forEach((story, index) => (story.sortOrder = index))
+        .sort((a, b) => sortItemsByForwardDirection(a, a.storyId, b, b.storyId, storyId, isUpForward))
+        .forEach((story, index) => story.sortOrder = index)
 
       return { ...state, sprints: copiedSprints, backlogCategories: copiedBacklogCategories}
     }
@@ -248,23 +181,21 @@ export default (state = initState, action) => {
 
       const tasks = copiedSprints.get(sprintId).stories.get(storyId).tasks
 
+      const changedTask = tasks.get(taskId)
+
       // 現在のステータス
-      const currentStatus = tasks.get(taskId).taskStatus
+      const currentStatus = changedTask.taskStatus
+
+      const isUpForward = newIndex - changedTask.sortOrder > 0
 
       // 新たな表示位置を指定
-      tasks.get(taskId).sortOrder = newIndex
+      changedTask.sortOrder = newIndex
 
       // タスク順を再設定する
       Array.from(tasks.values())
         .filter(task => task.taskStatus === currentStatus)
-        .sort((a, b) => {
-          // ユーザーにより変更されたタスクを優先的に前に並べる
-          if(a.sortOrder === b.sortOrder && a.taskId === taskId) return -1;
-          
-          // その他の場合は単純に昇順に並べる
-          return a.sortOrder - b.sortOrder
-        })
-        .forEach((task, index) => (task.sortOrder = index))
+        .sort((a, b) => sortItemsByForwardDirection(a, a.taskId, b, b.taskId, taskId, isUpForward))
+        .forEach((task, index) => task.sortOrder = index)
 
       return { ...state, sprints: copiedSprints, currentSprint: copiedSprints.get(sprintId)}
     }
@@ -281,13 +212,15 @@ export default (state = initState, action) => {
 
       const tasks = copiedSprints.get(sprintId).stories.get(storyId).tasks
 
-      const oldStatus = tasks.get(taskId).taskStatus
+      const changedTask = tasks.get(taskId)
+
+      const oldStatus = changedTask.taskStatus
 
       // ステータス変更
-      tasks.get(taskId).taskStatus = newStatus
+      changedTask.taskStatus = newStatus
 
       // 新ステータスでの表示位置を指定
-      tasks.get(taskId).sortOrder = newIndex
+      changedTask.sortOrder = newIndex
 
       // ステータスごとにタスク順を再設定する
       // 　1. 変更前のステータス
@@ -300,13 +233,7 @@ export default (state = initState, action) => {
       // 　2. 変更後のステータス
       Array.from(tasks.values())
         .filter(task => task.taskStatus === newStatus)
-        .sort((a, b) => {
-          // ステータス変更されたタスクを優先的に前に並べる
-          if(a.sortOrder === b.sortOrder && a.taskId === taskId) return -1;
-          
-          // その他の場合は単純に昇順に並べる
-          return a.sortOrder - b.sortOrder
-        })
+        .sort((a, b) => sortItems(a, a.taskId, b, b.taskId, taskId))
         .forEach((task, index) => (task.sortOrder = index))
 
       return { ...state, sprints: copiedSprints, currentSprint: copiedSprints.get(sprintId)}
@@ -328,4 +255,75 @@ export default (state = initState, action) => {
     } default:
       return state
   }
+}
+
+const copySprintsMap = srcSprintsMap => {
+  const copiedSprintsMap = new Map()
+
+  for (const [srcSprintId, srcSprint] of srcSprintsMap.entries()) {
+    const copiedStories = new Map()
+
+    for (const [srcStoryId, srcStory] of srcSprint.stories.entries()) {
+      const copiedTasks = new Map()
+
+      for (const [srcTaskId, srcTask] of srcStory.tasks.entries()) {
+        copiedTasks.set(srcTaskId, {...srcTask})
+      }
+
+      copiedStories.set(srcStoryId, {...srcStory, tasks: copiedTasks})
+    }
+
+    copiedSprintsMap.set(srcSprintId, {...srcSprint, stories: copiedStories})
+  }
+
+  return copiedSprintsMap
+}
+
+const copyBacklogCategoriesMap = srcBacklogCategoriesMap => {
+  const copiedBacklogCategoriesMap = new Map()
+
+  for (const [srcBacklogCategoryId, srcBacklogCategory] of srcBacklogCategoriesMap.entries()) {
+    const copiedStories = new Map()
+
+    for (const [srcStoryId, srcStory] of srcBacklogCategory.stories.entries()) {
+      const copiedTasks = new Map()
+
+      for (const [srcTaskId, srcTask] of srcStory.tasks.entries()) {
+        copiedTasks.set(srcTaskId, {...srcTask})
+      }
+
+      copiedStories.set(srcStoryId, {...srcStory, tasks: copiedTasks})
+    }
+
+    copiedBacklogCategoriesMap.set(srcBacklogCategoryId, {...srcBacklogCategory, stories: copiedStories})
+  }
+
+  return copiedBacklogCategoriesMap
+}
+
+const sortItemsByForwardDirection = (a, aId, b, bId, sortTargetId, isUpForward) => {
+  // 同じ表示順の場合、順番の変更方向によって変更対象（ストーリー、タスク）を優先的に前 or 後に並べる
+  if(a.sortOrder === b.sortOrder){
+    if (isUpForward) {
+      if(aId === sortTargetId) return 1
+      if(bId === sortTargetId) return -1
+    } else {
+      if(aId === sortTargetId) return -1
+      if(bId === sortTargetId) return 1
+    }
+  } 
+  
+  // その他の場合は単純に昇順に並べる
+  return a.sortOrder - b.sortOrder
+}
+
+const sortItems = (a, aId, b, bId, sortTargetId) => {
+  // 同じ表示順の場合、変更対象（ストーリー、タスク）を優先的に前に並べる
+  if(a.sortOrder === b.sortOrder){
+    if(aId === sortTargetId) return -1
+    if(bId === sortTargetId) return 1
+  } 
+  
+  // その他の場合は単純に昇順に並べる
+  return a.sortOrder - b.sortOrder
 }
