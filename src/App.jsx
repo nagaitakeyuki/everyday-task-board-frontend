e52348ae2141258837a96a2b52cb4c5948594f3f
nagaitakeyuki <goodingmorning@yahoo.co.jp>
2019-08-27 06:18:34 +0900

    Merge route setting to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React, {Fragment} from 'react'
 import {Link, HashRouter as Router} from 'react-router-dom'
-import Routes from './common/routes'
+import {Route, Switch} from 'react-router'
 import "antd/dist/antd.css"
+
+import SprintBacklogPage from './task_management/sprint_backlog/component/SprintBacklogPage'
+import TaskBoardPage from './task_management/task_board/component/TaskBoardPage'
 
 export default () => (
   <Router>
     <Fragment>
       <div className="container-fluid" style={{ marginTop: "10px" }}>
         <Link to={'/'}>Top</Link>
-        <Routes />
+        <Switch>
+          <Route path={'/'} component={SprintBacklogPage} exact={true} />
+          <Route path={'/sprints/:sprintId/task_board'} component={TaskBoardPage} exact={true} />
+        </Switch>
       </div>
     </Fragment>
   </Router>
