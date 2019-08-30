8a70a30b44d29cd61cbf12888b99f4e44b5051e1
nagaitakeyuki <goodingmorning@yahoo.co.jp>
2019-08-31 05:35:08 +0900

    Enable direct access to task board view.

--- src/task_management/task_board/component/TaskBoardPage.jsx
+++ src/task_management/task_board/component/TaskBoardPage.jsx
@@ -1,33 +1,47 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
+import { Spin } from 'antd'
 
 import TaskBoardTitle from './TaskBoardTitle'
 import TaskBoardTable from './TaskBoardTable'
-import Actions from '../taskBoardActions'
+import sprintBacklogActions from '../../sprint_backlog/sprintBacklogActions'
+import taskBoardActions from '../taskBoardActions'
 
 
 export default connect(state => (
-  { sprint: state.sprint.currentSprint }
+  { sprint: state.sprint.currentSprint,
+    isLoaded: state.sprint.sprints !== undefined }
 ))(
   class extends Component {
 
     componentDidMount() {
-      const { sprintId } = this.props.match.params
-      this.props.dispatch(Actions.swithSprint({ sprintId }))
+      if (!this.props.isLoaded) 
+        this.props.dispatch(sprintBacklogActions.getSprints())
     }
 
     render() {
+      if(this.props.isLoaded && !this.props.sprint) {
+        const { sprintId } = this.props.match.params
+        this.props.dispatch(taskBoardActions.swithSprint({ sprintId }))
+      }
+
       const sprint = this.props.sprint
 
       return (
-        <div style={{ paddingTop: "5px", fontSize: "0.8rem"}}>
-          {sprint ? (
-            <Fragment>
-              <TaskBoardTitle sprint={sprint} />
-              <TaskBoardTable stories={sprint.stories} />
-            </Fragment>
-          ): null}
-        </div>
+        <Fragment>
+          {this.props.isLoaded && sprint
+            ? 
+              <div style={{ paddingTop: "5px", fontSize: "0.8rem"}}>
+                {sprint ? (
+                  <Fragment>
+                    <TaskBoardTitle sprint={sprint} />
+                    <TaskBoardTable stories={sprint.stories} />
+                  </Fragment>
+                ): null}
+              </div>
+            : 
+              <div><Spin tip="Loading..."/></div>}
+        </Fragment>
       )
     }
   }
