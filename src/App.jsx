b83b7f300cf3f41368cff79c44c074d720e6e9bb
nagaitakeyuki <goodingmorning@yahoo.co.jp>
2019-08-31 05:34:12 +0900

    Use not ReloadPage but simple anchor tag.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {Route, Switch} from 'react-router'
 import "antd/dist/antd.css"
 
 import InitalLoadPage from './entire_load/InitalLoadPage'
-import ReloadPage from './entire_load/ReloadPage'
 import SprintBacklogPage from './task_management/sprint_backlog/component/SprintBacklogPage'
 import TaskBoardPage from './task_management/task_board/component/TaskBoardPage'
 import './App.css'
@@ -12,16 +11,15 @@ import './App.css'
 export default () => (
   <Router>
     <div style={{width: "100%", height: "100vh", padding: "10px"}}>
-      <Link to={'/reload'}>
+      <a href="/">
         <img src="imgs/logo.png" />
-      </Link>
+      </a>
       &nbsp;&nbsp;
       <Link to={'/running'}>進行中</Link>
       &nbsp;&nbsp;
       <Link to={'/closed'}>完了分</Link>
       <Switch>
         <Route path={'/'} component={InitalLoadPage} exact={true} />
-        <Route path={'/reload'} component={ReloadPage} exact={true} />
         <Route path={'/running'} render={() => <SprintBacklogPage isClosedView={false}/>} exact={true} />
         <Route path={'/closed'} render={() => <SprintBacklogPage isClosedView={true}/>} exact={true} />
         <Route path={'/sprints/:sprintId/task_board'} component={TaskBoardPage} exact={true} />
